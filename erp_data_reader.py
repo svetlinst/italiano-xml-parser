c3016f1ce647e5fd23bdf4f5a0b5a9660956ed70
svetlinst <svetlinst@gmail.com>
2022-08-26 19:12:52 +0300

    Add basic logging

--- erp_data_reader.py
+++ erp_data_reader.py
@@ -2,10 +2,20 @@ import os
 from pathlib import Path
 import xmltodict
 from models.erp_models import InvoiceParser
+import logging
 
 # Config path to directories
 project_directory = Path(os.path.dirname(os.path.abspath(__file__)))
 input_directory = Path(project_directory / 'input')
+
+# Configure the logger
+LOG_NAME = 'log.csv'
+logging.basicConfig(filename=LOG_NAME,
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
+logging.info("Logging execution for XML-Parser")
 
 for file in input_directory.iterdir():
     if file.suffix == '.xml':
@@ -14,10 +24,13 @@ for file in input_directory.iterdir():
             xml_doc = xmltodict.parse(fd.read())
 
         # Create an instance of the InvoiceParser object with the dictionary data
+
         invoice_parser = InvoiceParser(xml_doc)
 
         # Try to parse the XML file into an Invoice object(s)
         try:
             invoice_parser.parse_invoice_data()
+            logging.debug('Successfully parsed the file')
         except Exception as ex:
+            logging.exception('message')
             raise ValueError('Error parsing the data!')
